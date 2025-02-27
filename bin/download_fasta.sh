a33e39ef4d537e20bd1e8cdc6d7bfe85cfbeb539
mapo9 <mark.polster@qbic.uni-tuebingen.de>
2025-02-27 12:02:03 +0100

    fixed download bug

--- bin/download_fasta.sh
+++ bin/download_fasta.sh
@@ -55,12 +55,13 @@ if [ -z "$PROTEOMES" ]; then
     GENUS=$(echo "$ORGANISM" | awk -F'+' '{print $1}')
     echo $GENUS
     PROTEOMES=$(curl -s "https://rest.uniprot.org/proteomes/search?query=taxonomy_name:$GENUS+AND+proteome_type:reference&format=list")
-    echo $PROTEOMES
 
     for SPECIES_PROTEOME in $PROTEOMES; do
+        echo $SPECIES_PROTEOME
         curl "https://rest.uniprot.org/uniprotkb/stream?compressed=false&format=fasta&query=(proteome:$SPECIES_PROTEOME)" -o ${OUT_PATH}/${ORGANISM_NAME}_reference.fasta
 
-        if [[ -s ${OUT_PATH}/${GENUS}_reference.fasta ]]; then
+        # Check if related species FASTA file is non-empty
+        if [[ -s ${OUT_PATH}/${ORGANISM_NAME}_reference.fasta ]]; then
             echo "Successfully downloaded FASTA for $ORGANISM_NAME: ${ORGANISM_NAME}_${RELATED_ORGANISM}_reference.fasta"
             break
         fi
